799b2f5b6c1178f4ad1ae1503013bbea91281c2d
marynia1994 <martynazak17@gmail.com>
2023-01-20 15:27:12 +0100

    user_panel

--- it_zwierzak/user_panel/urls.py
+++ it_zwierzak/user_panel/urls.py
@@ -1,16 +1,11 @@
 from django.urls import path
 from . import views
 
-from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.edit import CreateView
+
 
 
 urlpatterns = [
-    path('rejestruj', CreateView.as_view(
-        template_name='user_panel/register.html',
-        form_class=UserCreationForm)),
-    path('loguj', views.login, name='loguj'),
-    path('wyloguj', views.logout, name='wyloguj'),
+    path('  ', views.login, name='  '),
 
 ]
 
--- it_zwierzak/user_panel/views.py
+++ it_zwierzak/user_panel/views.py
@@ -1,18 +1,1 @@
-from django.contrib.auth import login, logout
-from django.shortcuts import redirect
-from django.urls import reverse
-from django.contrib import messages
 
-def login(request):
-    from django.contrib.auth.forms import AuthenticationForm
-    if request.method == 'POST':
-        form = AuthenticationForm(request, request.POST)
-        if form.is_valid():
-            login(request, form.get_user())
-            messages.success(request, "Zostałeś zalogowany!")
-            return redirect(reverse('user_panel:index'))
-
-def logout(request):
-    logout(request)
-    messages.info(request, "Zostałeś wylogowany!")
-    return redirect(reverse('user_panel:index'))
