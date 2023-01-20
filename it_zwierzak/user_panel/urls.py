ce935c6e5b1d1650bbd2a27fce1d435bf6b6bf31
marynia1994 <martynazak17@gmail.com>
2023-01-20 02:37:14 +0100

    user_panel

--- it_zwierzak/user_panel/urls.py
+++ it_zwierzak/user_panel/urls.py
@@ -7,10 +7,11 @@ from django.views.generic.edit import CreateView
 
 urlpatterns = [
     path('rejestruj', CreateView.as_view(
-        template_name='rejestruj.html',
+        template_name='user_panel/register.html',
         form_class=UserCreationForm)),
-    path('loguj', views.loguj, name='loguj'),
-    path('wyloguj', views.wyloguj, name='wyloguj'),
+    path('loguj', views.login, name='loguj'),
+    path('wyloguj', views.logout, name='wyloguj'),
+
 ]
 
 
