37a8fc9920de78e9a3b77c28c25a85b491506573
Tykfa <xxcezary@gmail.com>
2023-01-20 12:45:35 +0100

    add database models and register in admin area

--- it_zwierzak/adoption_announcement/models.py
+++ it_zwierzak/adoption_announcement/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 
+
 class Announcement(models.Model):
     species = models.CharField(max_length=30)
     breed = models.CharField(max_length=30)
     name = models.CharField(max_length=15)
-    age = models.SmallIntegerField(20)
+    age = models.SmallIntegerField()
     health = models.TextField()
     description = models.TextField()
 
+    def __str__(self):
+        return f'{self.species} {self.name} - {self.age} lat'
+
+
 class ButtonQuestion(models.Model):
     button = models.ForeignKey(Announcement, on_delete=models.CASCADE)
+
+
+class Member(models.Model):
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=30)
+    birth_date = models.DateField()
+    email = models.EmailField(max_length=50)
+    phone_number = models.IntegerField()
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
