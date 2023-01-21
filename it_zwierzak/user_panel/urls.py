1ad57a3bffbc0090ccdc89150dd9eca472327248
marynia1994 <martynazak17@gmail.com>
2023-01-21 01:04:22 +0100

    user_panel

--- it_zwierzak/user_panel/urls.py
+++ it_zwierzak/user_panel/urls.py
@@ -3,11 +3,13 @@ from . import views
 
 
 
-
 urlpatterns = [
-    path('  ', views.login, name='  '),
-
+    path('my_profile', views.my_profile, name='my_profile'),
+    path('favourite', views.favourite, name='favourite'),
+    path('adoptions', views.adoptions, name='adoptions'),
+    path('user_data', views.user_data, name='user_data')
 ]
+
 
 
 
--- it_zwierzak/user_panel/views.py
+++ it_zwierzak/user_panel/views.py
@@ -1,1 +1,31 @@
+from django.shortcuts import render
 
+# Create your views here.
+
+
+def my_profile(request):
+    return render(
+        request,
+        'user_panel/my_profile.html'
+    )
+
+
+def adoptions(request):
+    return render(
+        request,
+        'user_panel/adoptions.html'
+    )
+
+
+def favourite(request):
+    return render(
+        request,
+        'user_panel/favourite.html'
+    )
+
+
+def user_data(request):
+    return render(
+        request,
+        'user_panel/user_data.html'
+    )
