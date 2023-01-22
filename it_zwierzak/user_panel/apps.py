1aec54dd18bbcbb2c4815364d8a7d5d58dbbb67f
marynia1994 <martynazak17@gmail.com>
2023-01-22 13:15:20 +0100

    user_panel

--- it_zwierzak/user_panel/apps.py
+++ it_zwierzak/user_panel/apps.py
@@ -1,8 +1,6 @@
-from django.conf.urls import patterns, url
-from homepage import views
+from django.apps import AppConfig
 
-urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'),
-        url(r'^register_profile/$', views.register_profile, name='register_profile'),
-        url(r'^(?P<username>\w+)/$', views.profile_page, name='user_profile'),
-        )
+
+class HomepageConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'user_panel'
