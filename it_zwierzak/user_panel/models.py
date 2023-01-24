9dacd930b956449002a9c781091ba59ec16a51e7
marynia1994 <martynazak17@gmail.com>
2023-01-24 16:03:20 +0100

    zmiany

--- it_zwierzak/user_panel/models.py
+++ it_zwierzak/user_panel/models.py
@@ -1,10 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.conf import settings
+
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE) # Delete profile when user is deleted
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+ user = models.OneToOneField(settings.AUTH_USER_MODEL,
+ on_delete=models.CASCADE)
+ date_of_birth = models.DateField(blank=True, null=True)
+ photo = models.ImageField(upload_to='users/%Y/%m/%d',
+ blank=True)
 
-    def __str__(self):
-        return f'{self.user.username} Profile'
+ def __str__(self):
+     return 'Profil użytkownika {}.'.format(self.user.username)
