1f8d5a37b64cb27eca3532eb57195c76454ec2f0
Tykfa <xxcezary@gmail.com>
2023-01-25 18:58:53 +0100

    create proper animal model with working photos

--- it_zwierzak/adoption_announcement/admin.py
+++ it_zwierzak/adoption_announcement/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Animal
+from .models import Animal, Announcement
 
 
 admin.site.register(Animal)
-
+admin.site.register(Announcement)
--- it_zwierzak/adoption_announcement/models.py
+++ it_zwierzak/adoption_announcement/models.py
@@ -1,20 +1,21 @@
 from django.conf import settings
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-class Announcment(models.Model):
-    gender = models.CharField(max_length=10)
-    color = models.CharField(max_length=20)
-    age = models.SmallIntegerField()
-    species = models.CharField(max_length=30)
-    breed = models.CharField(max_length=30)
-    name = models.CharField(max_length=15)
+class Announcement(models.Model):
+    Animal = models.ForeignKey('Animal', on_delete=models.CASCADE,default=None)
+    Animal_Bio = models.TextField(max_length=3000, default=' ')
 
-    health = models.TextField()
-    description = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+    added_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        limit_choices_to={'is_staff': True}, null=True
+    )
 
     def __str__(self):
-        return f'{self.species} {self.name} - {self.age} lat'
+        return f'{self.Animal}'
 
 
 class Animal(models.Model):
@@ -23,23 +24,18 @@ class Animal(models.Model):
         ('F', 'Żeńska'),
         ('O', 'Inna')]
 
-    SPECIES_CHOICES = [
-        ('dog', 'Pies'),
-        ('cat', 'Kot'),
-        ('rabbit', 'Królik'),
-        ('snake', 'Wonsz'),
-        ('other', 'Inne')
-    ]
 
-    species = models.CharField(choices=SPECIES_CHOICES, default='dog', max_length=10)  # TODO choice variable shows in En
+    species = models.CharField(max_length=10)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='M')
 
     name = models.CharField(max_length=20)
     color = models.CharField(max_length=100)
-    weight = models.FloatField(max_length=10, help_text='w kilogramach')
+    weight = models.FloatField(max_length=10, help_text='w kilogramach', validators=[MinValueValidator(0.1)])
 
     age = models.PositiveSmallIntegerField()
-    months = models.PositiveSmallIntegerField(help_text='fill when age is below 1 year', default=0)
+    months = models.PositiveSmallIntegerField(help_text='fill when age is below 1 year', default=0,
+                                              validators=[MaxValueValidator(12)], null=True, blank=True
+                                              )
 
     image = models.ImageField(null=True, blank=True, upload_to="images/")
 
