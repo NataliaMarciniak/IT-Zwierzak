f0d721474ab3620c0957ddbc2562be3627e91b92
Natalia <marciniak.natalia95@gmail.com>
2023-01-29 14:00:08 +0100

    customize animals_for_adoption

--- it_zwierzak/adoption_announcement/urls.py
+++ it_zwierzak/adoption_announcement/urls.py
@@ -6,8 +6,7 @@ urlpatterns = [
     path('adopted_animals', views.adopted, name='adopted_animals'),
     path('animals_for_adoption', Announcement.as_view(), name='animals_for_adoption'),
     path('article/<int:pk>', AnimalDetail.as_view(), name='animal_detail'),
-    path('announcement_detail', views.adoption_card, name='announcement_detail'),
-    path('adoption_application', AdoptionApplicationView.as_view(), name='adoption_application'),
+    path('adoption_form/<int:pk>', AdoptionApplicationView.as_view(), name='adoption_application'),
     path('confirmation_adoption_application', views.confirmation_adoption_application,
          name='confirmation_adoption_application'),
 ]
